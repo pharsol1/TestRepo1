13fb6e425f48255e27746a0bcb1b868b8f700ab7
pharsol1 <pharsol1@partner.jaguarlandrover.com>
2023-09-30 08:21:03 +0100

    script files

---
+++ deploy_app.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# usage: ./deployme.sh staging sddffafaafaf
+# license: public domain
+
+application_name="WebRide"
+environment_name="Webride-env-1"
+
+EB_BUCKET=webride-bucket-27092023
+
+VERSION="Sample"
+#ZIP="code.$VERSION.zip"
+ZIP="$VERSION.zip"
+
+aws configure set default.region eu-north-1
+
+# Zip up the Dockerrun file
+zip -r $ZIP Dockerrun.aws.json
+
+echo "Copying new zip to S3"
+aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
+
+# Create a new application version with the zipped up Dockerrun file
+echo "Creating new application version"
+aws elasticbeanstalk create-application-version --application-name $application_name \
+    --version-label $VERSION --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP
+
+# Update the environment to use the new application version
+echo "Updating environment"
+aws elasticbeanstalk update-environment --environment-name $environment_name \
+      --version-label $VERSION
